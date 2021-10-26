d9fe818a6075651d0f2bb27f6828c431d8dff3d4
Amit <amitkr24031995@gmail.com>
2021-10-26 18:39:03 +0530

    Adding zero ignore

---
+++ UC1tic_tac_toe/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC1tic_tac_toe
+{
+    class Program
+    {
+        public static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        public static int player = 1;
+        public static int flag = 0;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TIC_TAC_TOE Game");
+           
+        }
+    }
+}
